bdb07556aedd8565d94cbcd2106c58014bc81852
IsmailPR03~ <you@example.com>
2023-05-05 18:59:28 +0600

    server API created

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('the-recipe-room');
+});
+
+app.get('/chefs', (req, res) => {
+  res.send(chefs);
+});
+
+app.get('/chefs/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id)
+  const selectedChef = chefs.find(c => c.chef_id === id);
+  res.send(selectedChef);
+})
+
+app.listen(port, () => {
+  console.log(`the recipe room is running on ${port}`)
+})
