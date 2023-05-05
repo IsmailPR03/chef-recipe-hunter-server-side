cab6b69b323be7bbce688d765950dbb2bb03a9b7
IsmailPR03~ <you@example.com>
2023-05-05 19:01:40 +0600

    connect cehef data to index file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
+const chefs = require('./data/chef.json')
 
 app.use(cors());
 
